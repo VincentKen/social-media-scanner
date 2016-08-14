81b2fb61c6932fd0cf333b6b8d0920ab8552b685
Vincent Kenbeek <vkenbeek@gmail.com>
2016-08-14 11:44:31 +0200

    Added index.js

---
+++ index.js
@@ -0,0 +1,66 @@
+var jsdom = require("jsdom");
+
+/**
+ * Checks if url parameter is a valid URL
+ * @param  {string}  url URL to check
+ * @return {Boolean}     returns true if valid URL
+ */
+function isValidURL(url) {
+  return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
+}
+
+
+/**
+ * Scanner class
+ * @param {url} url The starting URL of the website to scan
+ * @class
+ */
+function Scanner (url) {
+  var mainURL = url;
+
+  var callbacks = {};
+
+  /**
+   * Set callbacks
+   * @param  {string}   on       Name of callback
+   * @param  {Function} callback Callback function
+   * @return {void}
+   */
+  this.on = function (on, callback) {
+    callbacks[on] = callback;
+  };
+
+  /**
+   * Start scanning
+   * @return {void}
+   */
+  this.start = function () {
+
+  };
+
+}
+
+/**
+* Social media scanner
+* @class
+*/
+function SocialMediaScanner () {
+
+  /**
+  * Create a scanner
+  * @param  {string} url The starting URL of the website to scan
+  * @return {Scanner}
+  */
+  this.scan = function (url) {
+    return new Scanner(url);
+  };
+
+}
+
+socialMediaScanner = new SocialMediaScanner();
+
+scanner = socialMediaScanner.scan("test");
+
+scanner.start();
+
+module.exports = Scanner;
