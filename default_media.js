a8c3ca5bdc8cbb1ffd3d2a469805ca04d148fa88
Vincent Kenbeek <vkenbeek@gmail.com>
2016-08-17 15:02:09 +0200

    added default media links

--- default_media.js
+++ default_media.js
@@ -2,5 +2,11 @@ module.exports = [
   "facebook.com/",
   "fb.com/",
   "twitter.com/",
-  "linkedin.com/"
+  "linkedin.com/",
+  "instagram.com/",
+  "pinterest.com/",
+  "google.com/",
+  "reddit.com/",
+  "tumblr.com/",
+  "vk.com/"
 ];
